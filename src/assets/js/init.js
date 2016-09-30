ee09887d5876bbeef65a946116c38cbf1669ee06
mike <dinhmq1@gmail.com>
2016-09-29 23:51:43 -0500

    update

--- src/assets/js/init.js
+++ src/assets/js/init.js
@@ -4,5 +4,8 @@
     $('.button-collapse').sideNav();
     $('.tooltipped').tooltip({delay: 50});
 
+  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
